1ed0fbf585fadec6cbbc4750b9d01c700dfbea36
ASAuz <asauz777@gmail.com>
2020-01-10 16:49:52 +0500

    noComments

--- src/app/components/full-kew/full-kew.component.ts
+++ src/app/components/full-kew/full-kew.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FullKewComponent implements OnInit {
 
+	ChartName: string;
+	MicroSxema: any;
+	OM: number;
+	Kondesator: number;
+	kondesatorlar: kondesatorlar;
+	Colors: colors;
+	options: string[];
+
+
   constructor() { }
 
   ngOnInit() {
+  	this.ChartName = 'kew title chart...';
+  	this.MicroSxema = 'TDA - 2003';
+  	this.OM = 500;
+  	this.Kondesator = 0.50;
+  	this.kondesatorlar = {
+  		uzgarmas: '330 om',
+			uzgaruvchan: '100 om'
+  	};
+  	this.Colors = {
+  		car: 'white',
+  		salon: 'black',
+  		wheels: 'silver'
+  	};
+  	this.options = ['ABS', 'AutoPilot'];
   }
 
+}
+
+interface colors {
+	car: string,
+	salon: string,
+	wheels: string,
+}
+
+interface kondesatorlar {
+	uzgaruvchan: string;
+	uzgarmas: string;
 }
